23c72903e39499adee443f03c21fc6552882c93f
yuheng <sc20yl2@leeds.ac.uk>
2022-11-01 22:55:30 +0800

    front-end implemented

--- blueprints/views.py
+++ blueprints/views.py
@@ -6,3 +6,11 @@ bp = Blueprint("views", __name__, url_prefix="/")
 @bp.route("/")
 def index():
     return render_template("index.html")
+
+@bp.route("/login")
+def login():
+    return render_template("login.html")
+
+@bp.route("/register")
+def register():
+    return render_template("register.html")
--- static/script/base.js
+++ static/script/base.js
@@ -10,17 +10,21 @@ function displayWindowSize(){
     var side = document.getElementById("side-menu");
     var container2 = document.getElementById("container-box");
     var center = document.getElementById("center-box");
+    var add_container = document.getElementById("add-container");
     container.style.height = "calc(100vh - 485px)"
     if (document.body.clientWidth < 750){
         side.style.width = "0px";
         container2.style.width = "100vw";
         side.style.zIndex = "100";
         center.style.width = "90vw";
+        add_container.style.width = "96vw";
     } else {
         side.style.width = "255px";
         center.style.width = "calc(100vw - 500px)";
         container2.style.width = "calc(100vw - 255px)";
+        add_container.style.width = "40%";
     }
+
 }
 
 function side() {
